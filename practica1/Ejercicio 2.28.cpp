f47628abf6d3851ae57de8546c3e6fc4c0f719b6
miguelConchacalla <iair.suico@ucsp.edu.pe>
2022-09-09 08:25:18 -0500

    semana3

--- practica1/Ejercicio 2.28.cpp
+++ practica1/Ejercicio 2.28.cpp
@@ -4,23 +4,28 @@ using namespace std;
 
 int main()
 {
-int numero,inverso;
+int numero,inverso,m,c,d,u;
     cout << "Ingrese un entero de 4 digitos:" << endl;
     cin >> numero;
 
     inverso=numero%10*1000;
     numero=numero/10;
+    u=inverso;
 
     inverso=inverso+numero%10*100;
     numero=numero/10;
+    d=inverso;
 
     inverso=inverso+numero%10*10;
     numero=numero/10;
+    c=inverso;
 
     inverso=inverso+numero;
+    m=inverso;
 
-
-    cout << "El numero invertido es:" << inverso << endl;
-
+// 6541
+// 6   5    
+//   cout << "El numero invertido es:" << inverso << endl;
+    cout<<  "El numero invertido es:" << u<<"\t" << d<<"\t" << c<<"\t" << m<< endl;
     return 0;
 }
